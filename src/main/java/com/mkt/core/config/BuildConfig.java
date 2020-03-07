037a57e008995a1e5e7917dac4bb69195997d528
Luis H <luislyly@hotmail.com>
2020-03-07 14:21:39 -0600

    refactor: Add profiles in configuration class

--- src/main/java/com/mkt/core/config/BuildConfig.java
+++ src/main/java/com/mkt/core/config/BuildConfig.java
@@ -2,11 +2,13 @@ package com.mkt.core.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
 
 import com.mkt.core.controller.BuildInfoController;
 import com.mkt.core.service.BuildInfoService;
 
 @Configuration
+@Profile({"dev","prod"})
 public class BuildConfig {
 	
 	@Bean
